39e60b9ffada2a7d28557e5bd58c8b14dbd15edb
Riddhi Shah <139042689+riddhi010@users.noreply.github.com>
2025-08-04 09:19:19 +0530

    Update App.js

--- mindbloom-frontend/src/App.js
+++ mindbloom-frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Cursor from "./components/cursor";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
