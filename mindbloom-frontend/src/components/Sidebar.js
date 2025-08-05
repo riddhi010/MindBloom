17ed06101ab014ad580f3949ff6c99ef277bdda3
Riddhi Shah <139042689+riddhi010@users.noreply.github.com>
2025-08-06 00:50:12 +0530

    Update Sidebar.js

--- mindbloom-frontend/src/components/Sidebar.js
+++ mindbloom-frontend/src/components/Sidebar.js
@@ -23,11 +23,11 @@ const Sidebar = () => {
       <div className={`sidebar ${isOpen ? "open" : ""}`}>
         <ul onClick={toggleSidebar}>
           <li><a href="/#/login">Login/Register</a></li>
-          <li><a href="/#">Home</a></li>
-          <li><a href="/#about">About</a></li>
-          <li><a href="/#testimonials">Testimonials</a></li>
-          <li><a href="/#gallery">Gallery</a></li>
-          <li><a href="/#contact">Contact</a></li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#testimonials">Testimonials</a></li>
+          <li><a href="#gallery">Gallery</a></li>
+          <li><a href="#contact">Contact</a></li>
         </ul>
       </div>
     </div>
