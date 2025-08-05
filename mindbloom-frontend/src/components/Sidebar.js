e4f2799d42704bdb84a35ef6c39134a3852be69a
Riddhi Shah <139042689+riddhi010@users.noreply.github.com>
2025-08-05 23:54:24 +0530

    Update Sidebar.js

--- mindbloom-frontend/src/components/Sidebar.js
+++ mindbloom-frontend/src/components/Sidebar.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import './Sidebar.css'; 
+import { HashLink as Link } from "react-router-hash-link";
+import './Sidebar.css';
 
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,26 +11,22 @@ const Sidebar = () => {
 
   return (
     <div>
-      
-      {/* Sidebar toggle button with icon */}
       <button className="sidebar-toggle" onClick={toggleSidebar}>
         {isOpen ? (
-          <i className="fa fa-times"></i> 
+          <i className="fa fa-times"></i>
         ) : (
-          <i className="fa fa-bars"></i> 
+          <i className="fa fa-bars"></i>
         )}
       </button>
 
-      {/* Sidebar */}
       <div className={`sidebar ${isOpen ? "open" : ""}`}>
         <ul>
-        <li><a href="/login">Login/Register</a></li>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#testimonials">Testimonials</a></li>
-        <li><a href="#gallery">Gallery</a></li>
-        <li><a href="#contact">Contact</a></li>
-        
+          <li><Link to="/login">Login/Register</Link></li>
+          <li><Link smooth to="#home">Home</Link></li>
+          <li><Link smooth to="#about">About</Link></li>
+          <li><Link smooth to="#testimonials">Testimonials</Link></li>
+          <li><Link smooth to="#gallery">Gallery</Link></li>
+          <li><Link smooth to="#contact">Contact</Link></li>
         </ul>
       </div>
     </div>
